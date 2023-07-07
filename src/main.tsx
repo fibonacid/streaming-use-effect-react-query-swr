aa1f7b95a0dfa3958872d50d3458aac805d6b976
Lorenzo Rivosecchi <lorenzo.rivosecchi@botshelf.ai>
2023-07-07 11:43:35 +0200

    create sw

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,31 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
+
+async function registerServiceWorker() {
+  if ("serviceWorker" in navigator) {
+    try {
+      const registration = await navigator.serviceWorker.register("/sw.js");
+      if (registration.installing) {
+        console.log("Service worker installing");
+      } else if (registration.waiting) {
+        console.log("Service worker installed");
+      } else if (registration.active) {
+        console.log("Service worker active");
+      }
+    } catch (error) {
+      if (error instanceof Error) {
+        console.error(`Registration failed with ${error.message}`);
+      }
+    }
+  }
+}
+
+void registerServiceWorker();
