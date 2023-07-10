a8d634662a0ed1b8934464738666dbfec58f4293
Lorenzo Rivosecchi <lorenzo.rivosecchi@botshelf.ai>
2023-07-10 10:47:37 +0200

    add abort controller

--- src/lib/swr.tsx
+++ src/lib/swr.tsx
@@ -1,4 +1,4 @@
-import { useId } from "react";
+import { useId, useState } from "react";
 import useSWR from "swr";
 import useSWRMutation from "swr/mutation";
 import { getCompletion } from "./shared";
@@ -6,6 +6,8 @@ import { getCompletion } from "./shared";
 export default function useCompletionSWR() {
   const id = useId();
   const { data, mutate } = useSWR<string>(["completion", id], null);
+  const [abortController, setAbortController] =
+    useState<AbortController | null>();
 
   const { trigger, isMutating, error } = useSWRMutation<
     void,
@@ -13,9 +15,18 @@ export default function useCompletionSWR() {
     [string, string],
     string
   >(["completion", id], async (_, { arg: prompt }) => {
-    for await (const token of getCompletion(prompt)) {
+    void mutate("", false);
+    if (abortController) {
+      abortController.abort();
+    }
+    const controller = new AbortController();
+    const signal = controller.signal;
+    setAbortController(controller);
+
+    for await (const token of getCompletion(prompt, signal)) {
       void mutate((prev) => (prev ? prev + token : token), false);
     }
+    setAbortController(null);
   });
 
   return [trigger, { data, error, isLoading: isMutating }] as const;
